2feacc39bf0369dbe9c1239edbe2b805e34dc704
Christopher Vachon <code@christophervachon.com>
2014-04-02 19:48:22 -0400

    Updated Documentation

--- jquery.syntaxhighlighter.js
+++ jquery.syntaxhighlighter.js
@@ -1,5 +1,5 @@
 /* 
-** jquery.syntaxhighlighter.js - v0.0.1 
+** jquery.syntaxhighlighter.js - v0.0.2
 ** Author: Christopher Vachon (code@christophervachon.com) 
 ** Build Date 2014-04-02 
 ** Repository: https://github.com/liaodrake/jquery.syntaxhighlighter.js
