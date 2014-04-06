9f5f1e0e632127d603b9bca8128da680c0bbae26
Christopher Vachon <code@christophervachon.com>
2014-04-06 16:31:51 -0400

    Allow multiple defined definitions

--- jquery.syntaxhighlighter.js
+++ jquery.syntaxhighlighter.js
@@ -16,64 +16,77 @@
 		var _settings = $.extend({
 			tab: "&nbsp;&nbsp;&nbsp;&nbsp;",
 			splitLinesRegEx: "\\r\\n|\\r|\\n|<br(?:\\s\\/)?>",
+			defaultDefinition: "code",
 			definitions: {
-				default: [{
-					class: "string",
-					pattern: "((?:\"|')[^\"']{0,}(?:\"|'))"
-				}, {
-					class: 'regex',
-					pattern: "(\\/[^\\/]+\\/[gim]{0,})"
-				}, {
-					class: 'const',
-					pattern: "(?:(var|new|function|private|if|else|for|in)\\s)"
-				}, {
-					class: 'operator',
-					pattern: "((?:[\\+\\-\\=\\!\\|\\:\\[\\]\\(\\)\\{\\}\\>\\<]|&amp;|&lt;|&gt;){1,}|(?:[^\\*\\/]\\/(?!\\/|\\*)))"
-				}, {
-					class: 'comment',
-					pattern: "\\/\\/[^\\(\\n|\\r)]+|\\/\\*|\\*\\/"
-				}]
+				"html": [
+					{
+						class: "html",
+						pattern: "((?:\<|&lt;)[^\>\&]+(\>|\&gt;))"
+					}
+				],
+				"code": [
+					{
+						class: "string",
+						pattern: "((?:\"|')[^\"']{0,}(?:\"|'))"
+					}, {
+						class: 'regex',
+						pattern: "(\\/[^\\/]+\\/[gim]{0,})"
+					}, {
+						class: 'const',
+						pattern: "(?:(var|new|function|private|if|else|for|in)\\s)"
+					}, {
+						class: 'operator',
+						pattern: "((?:[\\+\\-\\=\\!\\|\\:\\[\\]\\(\\)\\{\\}\\>\\<]|&amp;|&lt;|&gt;){1,}|(?:[^\\*\\/]\\/(?!\\/|\\*)))"
+					}, {
+						class: 'comment',
+						pattern: "\\/\\/[^\\(\\n|\\r)]+|\\/\\*|\\*\\/"
+					}
+				]
 			}
 		}, options );
 
 
 		return this.each(function() {
+			var _codeDefinitionTitle = $(this).attr('data-lang') || _settings.defaultDefinition;
+			var _table = $('<table>').addClass('syntax-highlighting')
+				.append($('<tr>')
+					.append($('<th>').prop('colspan',2).addClass('header').text(_codeDefinitionTitle))
+				).append($('<tr>')
+					.append($('<td>').addClass('gutter'))
+					.append($('<td>').addClass('code'))
+				);
+			var _thisCodeBlock = $(this);
+			getDefinitions(_codeDefinitionTitle,_settings).done(function(_definitions) {
 
-			var _table = $('<table>').addClass('syntax-highlighting').append($('<tr>')
-				.append($('<td>').addClass('gutter'))
-				.append($('<td>').addClass('code'))
-			);
+				var _computedRegExString = "";
+				for (var k in _definitions) { _computedRegExString += _definitions[k].pattern + "|"; }
+				_computedRegExString = "(" + _computedRegExString.replace(/\|$/, "") + ")";
+				var _computedRegEx = new RegExp(_computedRegExString, "gi");
 
-			var _definitions = _settings.definitions.default;
+				var _splitCodeRegEx = new RegExp(_settings.splitLinesRegEx, "gi");
+				var _code = _thisCodeBlock.html().split(_splitCodeRegEx);
+				var _numLines = (_code.length - 1);
+				//console.log(_code);
+				for (var i = 0; i <= _numLines; i++) {
+					var _hightlightedCode = _code[i].replace(/\t/g,_settings.tab);
+					_hightlightedCode = _hightlightedCode.replace(_computedRegEx, "<span class='found'>$1</span>");
+					_hightlightedCode = $('<div>').attr('data-line',i).html(_hightlightedCode || "&nbsp;");
+					_table.find('td.gutter').append($('<div>').attr('data-line',i).html(i + 1));
+					_table.find('td.code').append(_hightlightedCode);
+				}
+				_table.find('td.code .found').each(function applyHighlighting() {
+					$(this).removeClass('found').addClass(swapClassesForSyntaxHighlighting($(this),_definitions));
+				});
 
-			var _computedRegExString = "";
-			for (var k in _definitions) { _computedRegExString += _definitions[k].pattern + "|"; }
-			_computedRegExString = "(" + _computedRegExString.replace(/\|$/, "") + ")";
-			var _computedRegEx = new RegExp(_computedRegExString, "gi");
-
-			var _splitCodeRegEx = new RegExp(_settings.splitLinesRegEx, "gi");
-			var _code = $(this).html().split(_splitCodeRegEx);
-			var _numLines = (_code.length - 1);
-			//console.log(_code);
-			for (var i = 0; i <= _numLines; i++) {
-				var _hightlightedCode = _code[i].replace(/\t/g,_settings.tab);
-				_hightlightedCode = _hightlightedCode.replace(_computedRegEx, "<span class='found'>$1</span>");
-				_hightlightedCode = $('<div>').attr('data-line',i).html(_hightlightedCode || "&nbsp;");
-				_table.find('td.gutter').append($('<div>').attr('data-line',i).html(i + 1));
-				_table.find('td.code').append(_hightlightedCode);
-			}
-			_table.find('td.code .found').each(function applyHighlighting() {
-				$(this).removeClass('found').addClass(swapClassesForSyntaxHighlighting($(this),_definitions));
-			});
-
-			$(this).closest('pre').replaceWith(_table);
+				_thisCodeBlock.closest('pre').replaceWith(_table);
 
 
-			_table.find('td.code div[data-line]').each(function() {
-				$(this).closest('table').find('td.gutter div[data-line="' + $(this).attr('data-line') + '"]').height($(this).height());
-			});
+				_table.find('td.code div[data-line]').each(function() {
+					$(this).closest('table').find('td.gutter div[data-line="' + $(this).attr('data-line') + '"]').height($(this).height());
+				}); 
 
-		});
+			}); // close getDefinitions()
+		}); // close this.each()
 	};
 	function swapClassesForSyntaxHighlighting(_block,_patterns) {
 		for (var j in _patterns) {
@@ -83,5 +96,20 @@
 			}
 		}
 		return "found";
+	}
+	function getDefinitions(_definitionName,_settings) {
+
+		console.log("_definitionName: " + _definitionName);
+		console.log("_settings: " + _settings);
+
+		var _deff = $.Deferred();
+
+		if (_settings.definitions[_definitionName]) {
+			_deff.resolve(_settings.definitions[_definitionName]);
+		} else {
+			_deff.resolve(_settings.definitions[_settings.defaultDefinition]);
+		}
+
+		return _deff.promise();
 	}
 }( jQuery ));
